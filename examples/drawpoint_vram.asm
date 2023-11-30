2a22fc4d9484dda6ee39933824e3db386ffaf010
no382001 <102482527+no382001@users.noreply.github.com>
2023-11-30 16:38:08 +0100

    fixed drawpoint_wram example

--- examples/drawpoint_vram.asm
+++ examples/drawpoint_vram.asm
@@ -12,13 +12,21 @@ posy dw 100     ; Y position
 
 ; calculate the offset in video memory
 ; offset = Y * 320 + X
-mov ax, [posy]  ; Load Y position into AX
+mov ax, [posy]  ; load Y position into AX
 mov bx, 320     ; BX = 320 (screen width)
 mul bx          ; AX = Y * 320
 add ax, [posx]  ; AX = Y * 320 + X
 mov di, ax      ; DI = offset
 
-mov al, 12 ;red
+mov al, 1 ;red
 
 ; vram
 mov [es:di], al
+
+infloop: 
+    jmp infloop
+
+; otherwise text mode memory gets corrupted and writes random chars to the screen
+mov ah, 0
+mov al, 3
+int 10h
